d8820f240ddd4c952a66594e9474573db7dc0c18
rundimeco <rundimeco@gmail.com>
2021-04-13 12:04:28 +0200

    filter

--- json_to_vizu.py
+++ json_to_vizu.py
@@ -6,8 +6,9 @@ header = open_utf8("visualisation_tools/head.html")
 
 dic_titres = get_json("data/dico_titres_ID.json")
 
-out = "<table class = 'styled-table'>\n"
+out = ""
 table_header = []
+cpt = 0
 for ID, infos in dic_titres.items():
   if len(table_header) == 0:
     table_header = infos.keys()
@@ -18,7 +19,5 @@ for ID, infos in dic_titres.items():
   this_info = "</td><td>".join(elems)
   l = f"  <tr><td>{ID}</td><td>{this_info}</td></tr>\n"
   out +=l
-out+="</table>"
-
-out_html = re.sub("{{content}}", out, header)
+out_html = re.sub("{{content_table}}", out, header)
 write_utf8("test.html", out_html, verbose = True)
